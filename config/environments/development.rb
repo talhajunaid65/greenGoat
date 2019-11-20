ccc08026819f1670a8e487240b1d7c6b12f8f6a6
Waqas <mwaqas.shah@gmail.com>
2019-11-20 22:47:32 +0500

    server settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,28 +52,28 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: 'thegoat@greengoat.org'}
-  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
-  config.action_mailer.default_url_options = {:host => "localhost", :port => '3000', :protocol => "http"}
-  config.action_mailer.preview_path = "#{Rails.root}/tmp/mailers/previews"
-  config.cache_store = :redis_store, 'redis://localhost:6379/'
-
-
-
   # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.perform_deliveries = true
   # config.action_mailer.raise_delivery_errors = true
   # config.action_mailer.default_options = {from: 'thegoat@greengoat.org'}
-  # config.action_mailer.smtp_settings = {
-  #   address: 'smtp.gmail.com',
-  #   port: 587,
-  #   domain: "gmail.com",
-  #   user_name: 'test44149@gmail.com',
-  #   password: 'greengoat@1',
-  #   authentication: 'login',
-  #   enable_starttls_auto: true
-  # }
+  # config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+  # config.action_mailer.default_url_options = {:host => "localhost", :port => '3000', :protocol => "http"}
+  # config.action_mailer.preview_path = "#{Rails.root}/tmp/mailers/previews"
+  # config.cache_store = :redis_store, 'redis://localhost:6379/'
+
+
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'thegoat@greengoat.org'}
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: "gmail.com",
+    user_name: 'test44149@gmail.com',
+    password: 'greengoat@1',
+    authentication: 'login',
+    enable_starttls_auto: true
+  }
 
 end
