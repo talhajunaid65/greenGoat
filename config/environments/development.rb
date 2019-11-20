438b5e527a2fb150c57d658b1ef455ef8794ae08
Waqas <mwaqas.shah@gmail.com>
2019-11-20 23:26:06 +0500

    server settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_options = {from: 'thegoat@greengoat.org'}
-  config.action_mailer.default_url_options = { :host => 'greengoat.com' }
+  config.action_mailer.default_url_options = { :host => 'http://167.172.245.215/' }
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
