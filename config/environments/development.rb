02ad2d2cbe68ae6ecc8d9186faaf1d154b3080e4
Waqas <mwaqas.shah@gmail.com>
2019-12-19 02:09:17 +0500

    email change

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,18 +60,16 @@ Rails.application.configure do
   # config.action_mailer.default_url_options = {:host => "localhost", :port => '3000', :protocol => "http"}
   # config.action_mailer.preview_path = "#{Rails.root}/tmp/mailers/previews"
   # config.cache_store = :redis_store, 'redis://localhost:6379/'
- 
-  config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = {
-    :host => "http://167.172.245.215/"
-  }
+  Rails.application.routes.default_url_options[:host] = "greengoat.com"
+  config.action_mailer.default_options = {from: 'thegoat@greengoat.org'}
+  config.action_mailer.default_url_options = { :host => 'http://167.172.245.215/' }
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: 'thegoat@greengoat.org'}
   config.action_mailer.smtp_settings = {
     address:              'smtpout.secureserver.net',
-    port:                 25,
+    port:                 587,
     domain:               'greengoat.com',
     user_name:            'tech@greengoat.org',
     password:             'greenGoat123',
