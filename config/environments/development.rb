087c2435d4cc765676a1ecdd327d747bc97d6b96
Waqas <mwaqas.shah@gmail.com>
2019-12-19 01:44:34 +0500

    email change

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,7 +67,6 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: 'tech@greengoat.org'}
   config.action_mailer.smtp_settings = {
     address:              'smtpout.secureserver.net',
     port:                 25,
