8f45755460ef65598c2caf0439a5382895d28a0e
Waqas <mwaqas.shah@gmail.com>
2019-12-19 01:10:50 +0500

    email change

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,9 +61,9 @@ Rails.application.configure do
   # config.action_mailer.preview_path = "#{Rails.root}/tmp/mailers/previews"
   # config.cache_store = :redis_store, 'redis://localhost:6379/'
   Rails.application.routes.default_url_options[:host] = "greengoat.com"
+  config.action_mailer.default_options = {from: 'thegoat@greengoat.org'}
+  config.action_mailer.default_url_options = { :host => 'http://167.172.245.215/' }
 
-
- config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
