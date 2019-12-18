9c9d22e07d140a9370427c76347725ec3b5298c9
Waqas <mwaqas.shah@gmail.com>
2019-12-19 00:57:25 +0500

    email change

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,9 +64,6 @@ Rails.application.configure do
 
 
  config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = {
-    :host => "greengoat.com"
-  }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
