e469f6234e6d5dba8153a272c834ee71755125ec
Waqas <mwaqas.shah@gmail.com>
2019-12-20 02:15:53 +0500

    Home page images

--- app/controllers/home_images_controller.rb
+++ app/controllers/home_images_controller.rb
@@ -3,7 +3,7 @@ class HomeImagesController < ApplicationController
 	  home_images = HomeImage.all
 	  image_array = []
 		home_images.each do |home_image|
-			image_array << "http://167.172.245.215" + rails_blob_path(home_image.home_image, only_path: true) 
+			image_array << "http://167.172.245.215" + rails_blob_path(home_image.image, only_path: true) 
 		end
       render json: image_array, status: :ok
 	end	
