983e209357057f3e3a609cff393c2653f6c650d2
Talha Junaid <talha.junaid@algorepublic.com>
2020-01-24 23:45:03 +0500

    Indent and fix associations.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,12 +1,13 @@
 class Project < ApplicationRecord
-	belongs_to :user
-	has_many :tasks
-	has_many :products
-	has_many :group_items
-	accepts_nested_attributes_for :tasks, :allow_destroy => true
-	accepts_nested_attributes_for :group_items, :allow_destroy => true
+  belongs_to :admin_user
 
-	# validates :address, :presence => true, :uniqueness => true
-	enum type_of_project: [:gut, :full, :kitchen, :other]
-	enum status: [:not_pursuing, :appraisal_notes, :propsal, :contract]
+  has_many :tasks
+  has_many :products
+  has_many :group_items
+
+  accepts_nested_attributes_for :tasks, allow_destroy: true
+  accepts_nested_attributes_for :group_items, allow_destroy: true
+
+  enum type_of_project: [:gut, :full, :kitchen, :other]
+  enum status: [:not_pursuing, :appraisal_notes, :propsal, :contract]
 end
