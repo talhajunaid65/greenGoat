600c14e5fa15c51180cfde33a739ca888ed09f7b
Talha Junaid <talha.junaid@algorepublic.com>
2020-01-25 00:47:01 +0500

    Add first_name, last_name fields in form.

--- app/admin/admin_users.rb
+++ app/admin/admin_users.rb
@@ -1,9 +1,10 @@
 ActiveAdmin.register AdminUser do
-  permit_params :email, :password, :password_confirmation, :role
+  permit_params :first_name, :last_name, :email, :password, :password_confirmation, :role
 
   index do
     selectable_column
-    id_column
+    column :first_name
+    column :last_name
     column :email
     column :role
     column :created_at
@@ -13,9 +14,12 @@ ActiveAdmin.register AdminUser do
   filter :email
   filter :role
   filter :created_at
+  filter :first_name_or_last_name_cont, label: 'Search By Name'
 
   form do |f|
     f.inputs do
+      f.input :first_name
+      f.input :last_name
       f.input :email
       f.input :role
       f.input :password
