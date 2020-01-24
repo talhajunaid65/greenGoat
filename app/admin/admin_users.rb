961e3e9a13c4c8ff68a3cb8c8dd2adc27b561935
Talha Junaid <talha.junaid@algorepublic.com>
2020-01-24 23:33:27 +0500

    Add role field in admin_user form.

--- app/admin/admin_users.rb
+++ app/admin/admin_users.rb
@@ -17,10 +17,10 @@ ActiveAdmin.register AdminUser do
   form do |f|
     f.inputs do
       f.input :email
+      f.input :role
       f.input :password
       f.input :password_confirmation
     end
     f.actions
   end
-
 end
