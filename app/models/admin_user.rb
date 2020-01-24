3353d6d6e3800a1ea7a5802e6c58fab42da71639
Talha Junaid <talha.junaid@algorepublic.com>
2020-01-25 00:45:41 +0500

    Add architect role and association in models.

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -2,7 +2,11 @@ class AdminUser < ApplicationRecord
   devise :database_authenticatable, 
          :recoverable, :rememberable, :validatable
 
-  enum role: [:admin, :pm, :appraiser, :contractor]
+  enum role: [:admin, :pm, :appraiser, :contractor, :architect]
 
   has_many :created_notes, class_name: 'Note', foreign_key: 'created_by_id'
+
+  def to_s
+    [first_name, last_name].join(' ')
+  end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -6,6 +6,7 @@ class Project < ApplicationRecord
   belongs_to :pm, class_name: 'AdminUser', foreign_key: 'pm_id'
   belongs_to :appraiser, class_name: 'AdminUser', foreign_key: 'appraiser_id'
   belongs_to :contractor, class_name: 'AdminUser', foreign_key: 'contractor_id'
+  belongs_to :architect, class_name: 'AdminUser', foreign_key: 'architect_id'
 
   accepts_nested_attributes_for :tasks, allow_destroy: true
   accepts_nested_attributes_for :group_items, allow_destroy: true
