6182bc1b689cfd631aa8fc2175d3bc84f7676478
Talha Junaid <talha.junaid@square63.org>
2020-02-15 02:44:29 +0500

    REmove unused scopes.

--- app/admin/reports.rb
+++ app/admin/reports.rb
@@ -8,18 +8,8 @@ ActiveAdmin.register_page "Reports" do
     render 'admin/items/waiting_for_uninstallation'
   end
 
-  page_action :sale_waiting_delivery, method: :get do
-    @sales = Sale.waiting_delivery
-    render 'admin/sales/waiting_delivery'
-  end
-
   page_action :projects_approaching_demo, method: :get do
     @projects = Project.approaching_demo
     render 'admin/projects/approaching_demo'
-  end
-
-  page_action :sales_search do
-    @sales = params[:q].present? ? Sale.search(params[:q]) : []
-    render 'admin/sales/search'
   end
 end
