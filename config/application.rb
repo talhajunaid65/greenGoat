fbf2cf7808eaaa021304821c270651926ca9e5a4
Talha Junaid <talha.junaid@square63.org>
2020-02-18 00:17:56 +0500

    Set timezone to est.

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,8 @@ Bundler.require(*Rails.groups)
 
 module ListOfIngredients
   class Application < Rails::Application
+    Time.zone = 'Eastern Time (US & Canada)'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
