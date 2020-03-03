84a601986c2e34756513f643113e494883a23666
Talha Junaid <talha.junaid@square63.org>
2020-03-03 11:31:38 +0500

    Fix products api.

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,16 +1,16 @@
 class ProductSerializer < ActiveModel::Serializer
-	include Rails.application.routes.url_helpers
-  attributes :id, :title, :room_id,:category,:images, :need_uninstallation, :location, :appraised_value, :price, :description,
-			:count, :uom, :width, :height, :depth, :weight, :make, :model,
-			:serial, :sale_date, :pickup_date, :uninstallation_date
+  include Rails.application.routes.url_helpers
+  attributes :id, :title, :room_id,:category,:images, :need_uninstallation, :appraised_value, :price, :description,
+      :count, :uom, :width, :height, :depth, :weight, :make, :model,
+      :serial, :sale_date, :pickup_date, :uninstallation_date
 
-	def images
-		image_array = []
-		object.images.each do |image|
-			image_array << "http://167.172.245.215" + rails_blob_path(image, only_path: true) 
-		end
+  def images
+    image_array = []
+    object.images.each do |image|
+      image_array << "http://167.172.245.215" + rails_blob_path(image, only_path: true)
+    end
 
-		image_array	
+    image_array
   end
 
 end
