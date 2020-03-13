aed0bf6709281483af539c3890c7e593c8785bf1
Talha Junaid <talha.junaid@square63.org>
2020-03-13 23:39:52 +0500

    Remove unused gem.

--- Gemfile
+++ Gemfile
@@ -42,8 +42,6 @@ gem 'cancan'
 gem 'select2-rails'
 gem 'sassc-rails', '~> 2.1', '>= 2.1.1'
 gem "roo", "~> 2.8.0"
-gem 'pickadate-rails', "~> 3.5.6.0"
-
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -1,11 +1,8 @@
 //= require arctic_admin/base
-//= require pickadate/picker
-//= require pickadate/picker.date
 //= require select2
 
 //= require admin/products
 
 $(document).ready(function(){
-  $("#date_from, #date_to").pickadate();
   $( ".select2-dropdown" ).select2();
 });
