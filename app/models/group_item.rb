1a3617cd6f2e241960d310ec7be5face0caae85c
Talha Junaid <talha.junaid@square63.org>
2020-03-21 22:11:59 +0500

    Sanitize array input before saving to db.

--- app/models/group_item.rb
+++ app/models/group_item.rb
@@ -2,4 +2,12 @@ class GroupItem < ApplicationRecord
   belongs_to :project
 
   scope :find_by_product_id, -> (prodduct_id) { where(":product_id = ANY(product_ids)", product_id: prodduct_id) }
+
+  before_save :sanitize_array_input
+
+  private
+
+  def sanitize_array_input
+    self.product_ids = product_ids.reject(&:blank?)
+  end
 end
