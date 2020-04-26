8f1f22bb4fd6ca2002b25865f32085364cbfbdb9
Talha Junaid <talha.junaid@square63.org>
2020-04-26 18:01:39 +0500

    Fixed missing authenticate_user! method issue.

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   resources :pages
 
+  mount_devise_token_auth_for 'User', at: 'auth'
+
   namespace :api do
     namespace :v1 do
       mount_devise_token_auth_for 'User', at: 'auth', controllers: {
