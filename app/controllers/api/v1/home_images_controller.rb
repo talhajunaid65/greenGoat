01d50367672a7f06aa0cc580ab68963963624aeb
Talha Junaid <talha.junaid@square63.org>
2020-04-29 23:17:27 +0500

    Fix home image issue

--- app/controllers/api/v1/home_images_controller.rb
+++ app/controllers/api/v1/home_images_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::HomeImagesController < ApiController
 	  home_images = HomeImage.all
 	  image_array = []
 		home_images.each do |home_image|
-			image_array << "http://167.172.245.215" + rails_blob_path(home_image.image, only_path: true)
+			image_array << "http://3.84.100.107" + rails_blob_path(home_image.image, only_path: true)
 		end
       render json: image_array, status: :ok
 	end
