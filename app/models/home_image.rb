5346672df8af6d61f2969238d8aef14530031309
Talha Junaid <talha.junaid@square63.org>
2020-04-29 23:41:25 +0500

    Add attachment validation in home_image.

--- app/models/home_image.rb
+++ app/models/home_image.rb
@@ -1,3 +1,9 @@
 class HomeImage < ApplicationRecord
 	has_one_attached :image
+
+  validate :attached_image
+
+  def attached_image
+    errors.add(:image, 'please attached a file') unless image.attached?
+  end
 end
