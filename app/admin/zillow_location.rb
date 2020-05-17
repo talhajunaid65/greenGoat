2394fa319d9a1755a21500f5650e72975d5350f8
Talha Junaid <talha.junaid@square63.org>
2020-05-18 01:01:41 +0500

    FIx zillow location design issue.

--- app/admin/zillow_location.rb
+++ app/admin/zillow_location.rb
@@ -1,20 +1,22 @@
 ActiveAdmin.register ZillowLocation do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-permit_params :type_of_project, :address, :city, :state, :zip, :year_built,
-        :user_id, :tracking_id, :val_sf, :estimated_value,
-        :start_date
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+  permit_params :type_of_project, :address, :city, :state, :zip, :year_built, :user_id, :tracking_id, :val_sf,
+                :estimated_value, :start_date
 
-  
-  
-  
-
+  form do |f|
+    f.inputs do
+      f.input :user
+      f.input :type_of_project
+      f.input :address
+      f.input :city
+      f.input :state
+      f.input :zip
+      f.input :start_date, as: :datepicker
+      f.input :year_built
+      f.input :val_sf
+      f.input :estimated_value
+      f.input :latitude
+      f.input :longitude
+    end
+    f.actions
+  end
 end
